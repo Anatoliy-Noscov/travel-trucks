a7d6b3b01f01d1a225f6547a35ccf8aae4f25ed8
Anatoliy_Noscov <svodnuy7777@gmail.com>
2025-10-08 20:58:30 +0300

    add types component

---
+++ type/campers.ts
@@ -0,0 +1,64 @@
+export interface Camper {
+ id: string;
+ name: string;
+ price: number;
+ rating: number;
+ location: string;
+ adults: number;
+ children: number;
+ engine: string;
+ transmission: string;
+ form: string;
+ length: string;
+ width: string;
+ height: string;
+ tank: string;
+ consumption: string;
+ description: string;
+ gallery: string[];
+ reviews: Review[];
+ details: Details;
+ features: Features;
+}
+
+export interface Review {
+    reviewer_name: string;
+    reviewer_rating: number;
+    comment: string;
+}
+
+export interface Details {
+    form: string;
+    length: string;
+    width: string;
+    height: string;
+    tank: string;
+    consumption: string;
+}
+
+export interface Features {
+    airConditioner: number;
+    bathroom: number;
+    kitchen: number;
+    TV: number;
+    radio: number;
+    refrigerator: number;
+    microwave: number;
+    gas: number;
+    water: number;
+}
+
+export interface FilterParams {
+    location?: string;
+    form?: string;
+    airConditioner?: boolean;
+    kitchen?: boolean;
+    TV?: boolean;
+    radio: boolean;
+    refrigerator?: boolean;
+    microwave?: boolean;
+    gas?: boolean;
+    water?: boolean;
+    page?: number;
+    limit?: number;
+}
